bc517fbc6363fb7399076b41cf02825f35661787
MariaRamlochan <mariaramlochan@hotmail.com>
2020-01-27 11:18:03 -0500

    my class files

---
+++ Sandbox.java
@@ -0,0 +1,17 @@
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("\nHello, World, tjis is my Sandbox program");
+
+       final double PI = 3.14159;
+        double radius = 3.5;
+        double area;
+
+        area = PI * radius * radius;
+        System.out.println("\nThe area of the circle is " + area);
+
+    }
+
+}
